ebd726f9723a659f3a2e6474f61fffc368742f9f
shora051 <sahil.hora.26@gmail.com>
2025-04-12 19:26:33 -0400

    About Page + minor tweaks to base.html

--- home/views.py
+++ home/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'home/index.html')
+
+def about(request):
+    template_data = {}
+    template_data['title'] = 'About'
+    return render(request,
+                  'home/about.html')
