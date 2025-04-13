0e293d2b6d73e284e0bfb99fd03884ecb5e89813
shora051 <sahil.hora.26@gmail.com>
2025-04-13 16:59:35 -0400

    user auth functionality

---
+++ accounts/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.forms.utils import ErrorList
+from django.utils.safestring import mark_safe
+
+class CustomErrorList(ErrorList):
+    def __str__(self):
+        if not self:
+            return ''
+        return mark_safe(''.join([
+            f'<div class="alert alert-danger" role="alert"> {e}</div>' for e in self]))
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True, help_text="Required. Enter a valid email address.")
+
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+        for fieldname in ['username', 'email', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
+            self.fields[fieldname].widget.attrs.update({'class': 'form-control'})
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']  # Include email
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
