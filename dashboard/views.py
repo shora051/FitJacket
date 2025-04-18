924cc20d19f6200e67731605e0f6cd03fbd9632b
tarantech <taranusa@live.com>
2025-04-18 19:06:36 -0400

    create

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def dashboard_view(request):
+    return render(request, 'dashboard/dashboard.html')
+
+def create_plan_view(request):
+    return render(request, 'dashboard/createPlan.html')
--- trainquest/urls.py
+++ trainquest/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from dashboard import views as dashboard_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),  # home page routing
+    path('dashboard/', dashboard_views.dashboard_view, name='dashboard'),  # Added dashboard route
+    path('dashboard/createPlan', dashboard_views.create_plan_view, name='create_plan'),  # Added createPlan route
 ]
 
